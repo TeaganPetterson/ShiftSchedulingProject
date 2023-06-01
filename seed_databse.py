de7ca0439e365d307cc382a9821a53a49232a791
Teagan Petterson <teagan.petterson1@gmail.com>
2023-05-31 18:57:53 -0600

    initial commit

---
+++ seed_databse.py
@@ -0,0 +1,8 @@
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import (Flask, render_template, request, flash, session,
+                   redirect, jsonify)
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def shift_viewer():
+    return render_template("shiftEditor.html")
+
+if __name__ == "__main__":
+    # connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
