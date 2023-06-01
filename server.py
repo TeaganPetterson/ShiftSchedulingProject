124298ed1ed2f9772b15e760969935866438275d
Teagan Petterson <teagan.petterson1@gmail.com>
2023-06-01 13:59:29 -0600

    Added some clarifying comments

--- server.py
+++ server.py
@@ -2,13 +2,21 @@ from flask import (Flask, render_template, request, flash, session,
                    redirect, jsonify)
 from jinja2 import StrictUndefined
 
+import model
+import seed_database
+
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def shift_viewer():
-    return render_template("shiftEditor.html")
+    emps = seed_database.emps
+    emp_names = []
+    for emp in emps:
+        name = f"{emp.fname} {emp.lname}"
+        emp_names.append(name)
+    return render_template("shiftEditor.html", emp_names = emp_names)
 
 if __name__ == "__main__":
     # connect_to_db(app)
