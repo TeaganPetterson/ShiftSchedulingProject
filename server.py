7f80d1ab96ff90d0b7fa51c54b44ddb22ccd93cf
Teagan Petterson <teagan.petterson1@gmail.com>
2023-06-12 19:30:25 -0600

    got the ajax working

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import (Flask, render_template, request, flash, session,
                    redirect, jsonify)
 from jinja2 import StrictUndefined
 from datetime import datetime
-import model
+from model import db, connect_to_db, Assignment
 import crud
 
 app = Flask(__name__)
@@ -12,33 +12,27 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def shift_viewer():
-    # all_emps = crud.get_all_employees()
-    # emps = []
-    # for emp in all_emps:
-    #     emps.append(emp.id)
-    # session['employees'] = emps
-    # emps = session['employees']
-	# then add the session to the render
     shifts = crud.get_all_set_shifts()
     stations = crud.get_all_stations()
     if 'employees_on_shift' in session:
         emps = session.get('employees_on_shift')
     else:
         emps = ['nothing']
-    print(emps)
+    print(f"session shift id {type(session['shift_id'])}")
     return render_template("shiftEditor.html", 
                            emps = emps, 
                            shifts = shifts, 
-                           stations = stations)
+                           stations = stations,
+                           selected_date = session.get('selected_date'),
+                           selected_shift_id = int(session.get('shift_id')))
 
 @app.route('/switchShifts', methods=["POST"])
 def switch_shifts():
     selected_date = datetime.strptime(request.form.get("calendar"), '%Y-%m-%d').date()
-    print(f"selected date = {selected_date}")
-    print(type(selected_date))
     shift_id = request.form.get("selectedShift")
-    # print(shift_id)
-    # print(crud.shifts_in_range(shift_id))
+    print(f"shift_id {type(shift_id)}")
+    session['shift_id'] = shift_id
+    session['selected_date'] = request.form.get("calendar")
     shift_ids = crud.shifts_in_range(shift_id, selected_date)
     session['employees_on_shift'] = crud.get_employees_from_shift_ids(shift_ids)
     # change unassigned_emps to the session['employees'] variable
@@ -50,6 +44,37 @@ get employee by id for each in session
 return list of employee objects
 """
 
+@app.route('/makeAssignments', methods=["POST"])
+def make_assignments():
+    assignment_data = request.form
+    
+    # Access the selected_shift_id and selected_date from the request data
+    selected_shift_id = assignment_data.get('selectedShift')
+    selected_date = assignment_data.get('calendar')
+
+    # Iterate through the assignment data and create assignment objects
+    for station_id, employee_id in assignment_data.items():
+        if station_id not in ['selectedShift', 'calendar']:
+            assignment = Assignment(
+                station_id=station_id,
+                emp_id=employee_id,
+                shift_id=selected_shift_id,
+                date=selected_date
+            )
+            db.session.add(assignment)
+
+    try:
+        db.session.commit()
+        response = {'success': True, 'message': 'Assignments created successfully.'}
+    except Exception as e:
+        db.session.rollback()
+        response = {'success': False, 'message': 'Error creating assignments.', 'error': str(e)}
+
+    return jsonify(response)
+
+
+
+
 if __name__ == "__main__":
-    model.connect_to_db(app)
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
--- static/ajax.js
+++ static/ajax.js
@@ -2,21 +2,56 @@
 // Things like selecting a new date or shift will show the selected shifts' employees, and stuff like adding an employee to a new station will automatically save it to a database
 $(document).ready(function () {
 	// Attach event listeners to date and shift select elements
-	$('#calendar, #selectedShift').on('change', function () {
+	$('#calendar, #selectedShift').on('click', function (event) {
+		event.preventDefault()
 		var selectedDate = $('#calendar').val();
 		var selectedShift = $('#selectedShift').val();
-
+		console.log(selectedDate, selectedShift)
 		// Create the data object to send in the AJAX request
 		var requestData = {
 			calendar: selectedDate,
 			selectedShift: selectedShift
 		};
 
-		// Send the AJAX request
+		// Send the AJAX request to update the selected employees for the shift
 		$.ajax({
 			url: '/switchShifts',
 			type: 'POST',
 			data: requestData,
+			success: function (response) {
+				// I don't think I need anything in here...
+			},
+			error: function (error) {
+				console.error('AJAX request failed:', error);
+			}
+		});
+	});
+
+	// Attach event listener to the shiftAssignment form submit button
+	$('#shiftAssignment button[type="submit"]').on('click', function (event) {
+		event.preventDefault(); // Prevent the form from submitting
+
+		// Create an object to store the selected employees for each station
+		var assignmentData = {};
+
+		// Loop through each station and get the selected employee
+		$('#shiftAssignment select').each(function () {
+			var stationId = $(this).attr('id');
+			var employeeId = $(this).val();
+
+			// Add the employee ID to the assignmentData object
+			assignmentData[stationId] = employeeId;
+		});
+
+		// Add the selectedShift and calendar values to the assignmentData object
+		assignmentData.selectedShift = $('#selectedShift').val();
+		assignmentData.calendar = $('#calendar').val();
+
+		// Send the AJAX request to create the assignments
+		$.ajax({
+			url: '/makeAssignments',
+			type: 'POST',
+			data: assignmentData,
 			success: function (response) {
 				// Handle the response here if needed
 				// You can update the UI or perform any other actions based on the response
